70f50cd1f13e42267cd47f817725c245d767bc08
mariotong <424514340@qq.com>
2017-11-10 10:21:34 +0800

    宝贝上传功能完成

--- pages/admin/products.vue
+++ pages/admin/products.vue
@@ -14,7 +14,7 @@
         tr(v-for='item in products')
           td
             .img(v-for='image in item.images')
-              img(:src='imageCDN + image + "?imageView2/1/format/jpg/q/75/imageslim"')
+              img(:src='image + "?imageView2/1/format/jpg/q/75/imageslim"')
           td {{item.title}}
           td {{item.price}}
           td(v-html='item.intro')
