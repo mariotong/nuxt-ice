6464b57005e7bb2c9e139c803b46a0f8245ea27d
mariotong <424514340@qq.com>
2017-11-16 17:00:22 +0800

    爬取教育的信息

--- server/api/edu.js
+++ server/api/edu.js
@@ -8,3 +8,55 @@ export async function saveIndexJson(data) {
     writeFileSync('./eduWikiJson/index.json', JSON.stringify(data, null, 2), 'utf8')
   }
 }
+
+export async function saveListHomeJson(data) {
+  if(existsSync('./eduWikiJson/listHome.json')) {
+    console.log('已经存在')
+  } else {
+    writeFileSync('./eduWikiJson/listHome.json', JSON.stringify(data, null, 2), 'utf8')
+  }
+}
+
+export async function saveListOtherJson(data, _id) {
+  if(existsSync(`./eduWikiJson/listHome${_id}.json`)) {
+    console.log('已经存在')
+  } else {
+    writeFileSync(`./eduWikiJson/listHome${_id}.json`, JSON.stringify(data, null, 2), 'utf8')
+  }
+}
+
+export async function saveCourselistJson(data, query) {
+  let { type, sort, courseId, selectScreenStr } = query
+  let name = (type + sort + encodeURIComponent(selectScreenStr) + courseId + '')
+  console.log(query)
+  console.log(name)
+  if(existsSync(`./eduWikiJson/${name}.json`)) {
+    console.log('已经存在')
+  } else {
+    writeFileSync(`./eduWikiJson/${name}.json`, JSON.stringify(data, null, 2), 'utf8')
+  }
+}
+
+export async function saveCourseDetailJson(data, _id) {
+  if(existsSync(`./eduWikiJson/courseDetail${_id}.json`)) {
+    console.log('已经存在')
+  } else {
+    writeFileSync(`./eduWikiJson/courseDetail${_id}.json`, JSON.stringify(data, null, 2), 'utf8')
+  }
+}
+
+export async function saveListhomeFilterJson(data, query) {
+  if(existsSync(`./eduWikiJson/listhomeFilter.json`)) {
+    console.log('已经存在')
+  } else {
+    writeFileSync(`./eduWikiJson/listhomeFilter.json`, JSON.stringify(data, null, 2), 'utf8')
+  }
+}
+
+export async function cartListJson(data, query) {
+  if(existsSync(`./eduWikiJson/cartListJson.json`)) {
+    console.log('已经存在')
+  } else {
+    writeFileSync(`./eduWikiJson/cartListJson.json`, JSON.stringify(data, null, 2), 'utf8')
+  }
+}
--- server/routes/edu.js
+++ server/routes/edu.js
@@ -18,7 +18,124 @@ export class EduController {
 
     api.edu.saveIndexJson(data)
 
-    console.log(data)
+    ctx.body = data
+  }
+
+  @get('/listhome')
+  async getListHome(ctx, next) {
+
+    let { data } = await axios({
+      url: `${apiUrl}/listhome`,
+      method: ctx.method,
+      headers: ctx.header
+    })
+
+    api.edu.saveListHomeJson(data)
+
+    ctx.body = data
+  }
+
+  @get('/listhome/:_id')
+  async getListOther(ctx, next) {
+    const { params } = ctx
+    const { _id } = params
+
+    let { data } = await axios({
+      url: `${apiUrl}/listhome/${_id}`,
+      method: ctx.method,
+      headers: ctx.header
+    })
+
+    api.edu.saveListOtherJson(data, _id)
+
+    ctx.body = data
+  }
+
+  @get('/courseDetail/index/:_id')
+  async getCourseDetail(ctx, next) {
+    const { params } = ctx
+    const { _id } = params
+
+    let { data } = await axios({
+      url: `${apiUrl}/courseDetail/index/${_id}`,
+      method: ctx.method,
+      headers: ctx.header
+    })
+
+    api.edu.saveCourseDetailJson(data, _id)
+
+    ctx.body = data
+  }
+
+  @get('/isexistCard/:_id')
+  async isexistCard(ctx, next) {
+    const { params } = ctx
+    const { _id } = params
+
+    ctx.body = {
+      data: null,
+      msg: 0,
+      status: false
+    }
+  }
+
+  @get('/courselist')
+  async getCourselist(ctx, next) {
+    const { query } = ctx
+    const { offset, limit, type, sort, courseId, selectScreenStr } = query
+
+    let { data } = await axios({
+      url: `${apiUrl}/courselist`,
+      method: ctx.method,
+      headers: ctx.header,
+      params: query
+    })
+
+    api.edu.saveCourselistJson(data, query)
+
+    ctx.body = data
+  }
+
+  @get('/listhome/filter/data')
+  async getListHomeFilterData(ctx, next) {
+    let { data } = await axios({
+      url: `${apiUrl}/listhome/filter/data`,
+      method: ctx.method,
+      headers: ctx.header
+    })
+
+    api.edu.saveListhomeFilterJson(data)
+
+    ctx.body = data
+  }
+
+
+  @get('/cartList')
+  async getCartListData(ctx, next) {
+    const { query } = ctx
+    let { data } = await axios({
+      url: `${apiUrl}/cartlist`,
+      method: ctx.method,
+      headers: ctx.header,
+      params: query
+    })
+
+    api.edu.cartListJson(data)
+
+    ctx.body = data
+  }
+
+  @post('/longin')
+  async postLogin(ctx, next) {
+    const { body } = ctx.request
+    let { data } = await axios({
+      url: `${apiUrl}/longin`,
+      method: ctx.method,
+      headers: ctx.header,
+      data: body
+    })
+
+    api.edu.cartListJson(data)
 
     ctx.body = data
   }
